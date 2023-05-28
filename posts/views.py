fb84ada5b9903078588f1d11d5ae5d27ef1bce5c
steven <steven.immune@gmail.com>
2023-05-27 17:25:41 -0700

    update code plus html

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import post
 # Create your views here.
+def post_list(request):
+    data = post.objects.all()
+
+    return render(request,'posts.html',{'posts':data})
+
+
+
+
+
+def post_detail(request,id_post):
+    data = post.objects.get(id=id_post)
+    return render(request,'detial.html',{'posts':data})
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import post_list , post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/' , post_list)
 ]
